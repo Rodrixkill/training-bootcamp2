92666fac4e37534ea77a7b890bce6942b05651e6
rvillarroel <rodrigo.villarroel.m@gmail.com>
2022-10-24 17:08:18 -0400

    This is a commit new permissions

---
+++ src/main/java/client.java
@@ -0,0 +1,14 @@
+public class client {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 8;
+
+        if(a > b){
+            a*=2;
+            System.out.println(a);
+        }else {
+            b-=2;
+            System.out.println(b);
+        }
+    }
+}
